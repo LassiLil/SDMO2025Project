1694562f69d24344074ea09f0efbb2165e90bd5f
Lassi Liljeström <Lassi.Liljestrom@student.oulu.fi>
2025-10-20 15:20:03 +0300

    clean test file

--- test_functions.py
+++ test_functions.py
@@ -1,19 +1,5 @@
 import unittest
 from functions import containsNameAndInitial, process
-print("testi")
-'''
-ANIRUDDHA ADAK   aniruddhaadak80@gmail.com,
-Aniruddha Bhattacharjee    aniruddha97bhatt@gmail.com
-0.6486486486486487,0.6451612903225806,1.0,0.23529411764705888,False,True,False,False
-'''
-
-'''
-print(containsNameAndInitial("","",""))
-print(containsNameAndInitial("etuliite","a","toinenNimi"))
-print(containsNameAndInitial("aniruddha97bhatt","a","aniruddha"))
-print(containsNameAndInitial("aaaa","b","aaaa"))
-print(containsNameAndInitial("akuankka","a","ankka"))
-'''
 
 class TestContainsNameAndInitial(unittest.TestCase):
 
